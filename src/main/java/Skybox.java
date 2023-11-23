30371eba6d9526c590bc4a627746ca01cc37608e
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-11-23 23:33:29 +0200

    commit

--- src/main/java/Skybox.java
+++ src/main/java/Skybox.java
@@ -1,5 +1,7 @@
 
 
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
@@ -26,10 +28,33 @@ public class Skybox {
 
         GL30.glBindBuffer(GL20.GL_ARRAY_BUFFER, vbo);
         GL30.glBufferData(GL20.GL_ARRAY_BUFFER, vertices, GL20.GL_STATIC_DRAW);
+        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+        GL20.glEnableVertexAttribArray(0);
 
-//        GL20
+        GL30.glBindBuffer(GL20.GL_ARRAY_BUFFER, 0);
+
+        GL30.glBindVertexArray(0);
+
+        // Load skybox texture
+        texture = TextureLoader.loadCubeMap("textures/skybox/skybox", "jpg");
     }
 
     public void render(int shaderProgram) {
+        GL20.glUseProgram(shaderProgram);
+
+        // Disable depth testing to render the skybox behind everything else
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+
+        GL30.glBindVertexArray(vao);
+
+        GL13.glActiveTexture(GL13.GL_TEXTURE0);
+        GL11.glBindTexture(GL13.GL_TEXTURE_CUBE_MAP, texture);
+
+        GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 6);
+
+        GL30.glBindVertexArray(0);
+
+        // Enable depth testing again for the rest of the scene
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
     }
 }
